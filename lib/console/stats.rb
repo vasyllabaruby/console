e8ee7f51ff238df50e6ea960ce4072039d2c4399
Vasyl <vasyl.laba@rubygarage.org>
2022-06-24 12:14:26 +0300

    Get statistic headings and rows from gem

--- lib/console/stats.rb
+++ lib/console/stats.rb
@@ -11,12 +11,10 @@ module Console
 
     def self.print_table(game)
       format = '%-8s %-8s %-15s %-15s %-15s %-15s %s'
-      puts format(format, 'Rating', 'Name', 'Difficulty', 'Attempts Total', 'Attempts Used', 'Hints Total',
-                  'Hints Used')
+      puts format(format,'Rating', *Codebreaker::Player::HEADING)
       i = 0
       game.statistic.each do |player|
-        puts format(format, i += 1, player.name, player.difficulty, player.attempts_total, player.attempts_used,
-                    player.hints_total, player.hints_used)
+        puts format(format, i += 1, *player.rows)
       end
     end
   end
