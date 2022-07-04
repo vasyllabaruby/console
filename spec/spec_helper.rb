98ab718271b97cd933320377824fd6535c2811f6
Vasyl <vasyl.laba@rubygarage.org>
2022-07-04 12:57:46 +0300

    Configure because circleci

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,19 @@
 # frozen_string_literal: true
 
-require 'stringio'
 require 'simplecov'
 require 'codebreaker/game'
 SimpleCov.start
-SimpleCov.minimum_coverage 90
 
-require 'lib/console/console_viewer'
+require 'console/console_viewer'
 
 RSpec.configure do |config|
-  config.expect_with :rspec do |expectations|
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = '.rspec_status'
 
-  config.mock_with :rspec do |mocks|
-    mocks.verify_partial_doubles = true
-  end
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.disable_monkey_patching!
 
-  config.shared_context_metadata_behavior = :apply_to_host_groups
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
 end
