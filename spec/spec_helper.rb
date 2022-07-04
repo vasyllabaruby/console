facb1b5321c883595b93091f86605311210877d2
Vasyl <vasyl.laba@rubygarage.org>
2022-07-04 12:45:18 +0300

    Configure because circleci

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,8 +5,6 @@ require 'simplecov'
 SimpleCov.start
 SimpleCov.minimum_coverage 90
 
-require_relative '../autoload'
-
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
